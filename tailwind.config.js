08230aecc4335bb844b18efa09566612d82ee406
Sanaz Bahmani <sanazbahmani35@gmail.com>
2024-09-14 11:21:54 +0330

    Set up project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        redHatText: ["Red Hat Text"],
+      },
+      colors: {
+        primary: "hsl(14, 86%, 42%)",
+        secondary: "hsl(159, 69%, 38%)",
+        "Rose-50": "hsl(20, 50%, 98%)",
+        "Rose-100": "hsl(13, 31%, 94%)",
+        "Rose-300": "hsl(14, 25%, 72%)",
+        "Rose-400": "hsl(7, 20%, 60%)",
+        "Rose-500": "hsl(12, 20%, 44%)",
+        "Rose-900": "hsl(14, 65%, 9%)",
+      },
+    },
   },
   plugins: [],
 };
