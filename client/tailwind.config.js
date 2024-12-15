8504e094faee7ccd664dd275e01977ea65f63930
Sanaz Bahmani <sanazbahmani35@gmail.com>
2024-12-15 15:41:54 +0330

    Config tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: "hsl(14, 86%, 42%)",
+      secondary: "hsl(159, 69%, 38%)",
+      "rose-50": "hsl(20, 50%, 98%)",
+      "rose-100": "hsl(13, 31%, 94%)",
+      "rose-300": "hsl(14, 25%, 72%)",
+      "rose-400": "hsl(7, 20%, 60%)",
+      "rose-500": "hsl(12, 20%, 44%)",
+      "rose-900": "hsl(14, 65%, 9%)",
+    },
+    extend: {
+      fontFamily: {
+        sans: ['"Red Hat Text"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
