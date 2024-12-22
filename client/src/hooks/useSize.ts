c1d60a90587447e7a0842e3b7f4c6b4cc8b9b6ab
Sanaz Bahmani <sanazbahmani35@gmail.com>
2024-12-22 15:33:25 +0330

    Implement the useSize hook

---
+++ client/src/hooks/useSize.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const useSize = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const windowWidthHandler = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", windowWidthHandler);
+
+    return () => {
+      window.removeEventListener("resize", windowWidthHandler);
+    };
+  }, []);
+
+  return windowWidth;
+};
+
+export default useSize;
