977a7ccdcf57912e91cc0f4ecdf4e88a7d34362f
Sanaz Bahmani <sanazbahmani35@gmail.com>
2024-12-22 15:33:56 +0330

    Display images based on the screen size

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,30 +1,30 @@
 // components
 import { IconButton } from "./IconButton";
+// hooks
+import useSize from "../hooks/useSize";
 // types
-import {Product} from '../types'
+import { Product } from "../types";
 // icons
 import cartIcon from "../assets/icons/icon-add-to-cart.svg";
 
 interface CardProps {
-  product: {
-    id: number;
-    image: {
-      mobile: string;
-      tablet: string;
-      desktop: string;
-      thumbnail: string;
-    };
-    name: string;
-    category: string;
-    price: number;
-  };
+  product: Product;
 }
 
 export const Card = ({ product }: CardProps) => {
+  const windowWidth = useSize();
+
+  const productImage =
+    windowWidth < 768
+      ? product.image.mobile
+      : windowWidth < 1024
+      ? product.image.tablet
+      : product.image.desktop;
+
   return (
-    <div>
+    <div className="">
       <div>
-        <img src={product.image.mobile} alt="" />
+        <img src={productImage} alt="" className="rounded-md" />
         <IconButton icon={cartIcon} text="Add to Cart" alt="cart" />
         <p>{product.category}</p>
         <p>{product.name}</p>
--- client/src/components/CardList.tsx
+++ client/src/components/CardList.tsx
@@ -8,7 +8,7 @@ interface CardListProps {
 
 export const CardList = ({ products }: CardListProps) => {
   return (
-    <ul>
+    <ul className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3">
       {products.map((product) => (
         <Card key={product.id} product={product} />
       ))}
